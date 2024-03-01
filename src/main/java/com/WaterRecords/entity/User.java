7b8bcf392c5337932535e656c9183d2e57a09aa6
Salah <theonegreat0@gmail.com>
2024-03-01 18:05:56 +0100

    user class added

---
+++ src/main/java/com/WaterRecords/entity/User.java
@@ -0,0 +1,30 @@
+package com.WaterRecords.entity;
+
+import javax.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<WaterRecord> waterRecords;
+
+}
